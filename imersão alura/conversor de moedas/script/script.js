c4b9a07f9b9bd2bab2e6e02e88b88aca66129068
ValeriaRodrigues-Dev <114243387+ValeriaRodrigues-Dev@users.noreply.github.com>
2023-11-13 19:50:13 -0300

    modelo 1

---
+++ imersão alura/conversor de moedas/script/script.js
@@ -0,0 +1,34 @@
+
+// Primeira parte - aula
+var valoremdolar = 64;
+var valoremreal = 5.32;
+resultado = valoremdolar * valoremreal;
+
+alert("Cotação do dólar: " + valoremdolar + "\nValor em real: " + valoremreal);
+alert(" ---------Conversão---------" + "\nValor: R$ " + resultado.toFixed(2));
+ 
+/* 
+//Desafio Gui
+var cotbitcoin = 5;
+var valoreal = 179342.13;
+valorconvertido = cotbitcoin * valoreal;
+
+alert("Valor Bitcoin: " + cotbitcoin + "\nValor em real: " + valoreal);
+alert(" ---------Conversão---------" + "\nValor: R$ " + valorconvertido);
+
+ */
+
+/*
+// DESAFIO RAFA
+var valoremdolar = 64;
+var valoremreal = 5.32;
+resultado = valoremdolar * valoremreal;
+nome = "Valéria"
+
+alert("Cotação do dólar: " + valoremdolar + "\nValor em real: " + valoremreal);
+alert(" ---------Conversão---------" + "\nSolicitante: " + nome + "\nValor: R$ " + resultado.toFixed(2)) 
+*/
+
+
+
+
